6502b224b4f91d366357ae2536234c8a3fcf5e60
Rohit Jain <rohitjain.nsit@gmail.com>
2016-02-07 23:01:25 -0500

    Removed POS tag hw and updated readme with links

--- pos tagger/src/edu/berkeley/nlp/math/GradientMinimizer.java
+++
@@ -1,8 +0,0 @@
-package edu.berkeley.nlp.math;
-
-/**
- * @author Dan Klein
- */
-public interface GradientMinimizer {
-  double[] minimize(DifferentiableFunction function, double[] initial, double tolerance);
-}
